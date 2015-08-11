a9cb0ecec19125ad4b7bf42ad1dc84bfa7da8bfe
minionprocyk <peter.procyk@gmail.com>
2015-08-11 14:21:26 -0400

    fixed untracked files

--- src/Scripts/TestScript.java
+++ src/Scripts/TestScript.java
@@ -1,6 +1,7 @@
 package Scripts;
 import org.powerbot.script.PollingScript;
 import org.powerbot.script.Script.Manifest;
+import org.powerbot.script.Tile;
 import org.powerbot.script.rt6.ClientContext;
 
 import Constants.ObjectName;
@@ -9,6 +10,7 @@ import Engines.FightingEngine;
 @Manifest(name = "Test", description = "We do crazy things", properties = "client=6; topic=0;")
 public class TestScript  extends PollingScript<ClientContext>{
 	String[] rocks = new String[]{ObjectName.COPPER_ROCKS, ObjectName.TIN_ROCKS, ObjectName.IRON_ROCKS};
+	Tile tile = new Tile(3381, 3270, 0);
 	public void poll() {	
 		switch(getState())
 		{
